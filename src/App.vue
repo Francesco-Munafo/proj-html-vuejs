68fc9fee8c88fb92932e8a8fd536be463f1b46b2
Francesco-Munafo <acquamarea1@gmail.com>
2023-10-03 21:10:14 +0200

    Add header, hero and courses sections

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,126 @@
 
 
 <template>
-    <header>
+    <header class="d-flex justify-content-between p-4">
+        <div class="header_logo">
+            <img width="300" src="./assets/img/theme_eduprime_logo.png" alt="">
+        </div>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+                    <ul class="navbar-nav">
+
+                        <li class="nav-item dropdown px-1">
+                            <a class="nav-link dropdown-toggle txt_white" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Home
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Lorem.</a></li>
+                                <li><a class="dropdown-item" href="#">Lorem.</a></li>
+                                <li><a class="dropdown-item" href="#">Lorem.</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown px-1">
+                            <a class="nav-link dropdown-toggle txt_white" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Courses
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item px-1">
+                            <a class="nav-link txt_white" href="#">About Us</a>
+                        </li>
+
+                        <li class="nav-item dropdown px-1">
+                            <a class="nav-link dropdown-toggle txt_white" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                News
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item dropdown px-1">
+                            <a class="nav-link dropdown-toggle txt_white" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Pages
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item px-1">
+                            <a class="nav-link txt_white" href="#">Conctact</a>
+                        </li>
+                        <li class="nav-item px-1">
+                            <a class="nav-link txt_white" href="#">Purchase</a>
+                        </li>
+                    </ul>
+                    <button class="rounded-5 border-0 p-2 px-3 ms-4">VIEW CONCTACTS</button>
+                </div>
+            </div>
+        </nav>
 
     </header>
 
     <main>
+        <section id="jumbotron" class="d-flex justify-content-center text-center pt-5">
+            <div class="col-4">
+                <h1 class="fw-bold ls_1">Key to your success</h1>
+                <p class=" fs-5">EduPrime is the most versatile WordPress theme for educational purposes, showcasing
+                    universities, courses, secondary schools etc.</p>
+                <div class="d-flex my-5 justify-content-center">
+                    <button class="bg_warning txt_white fw-medium rounded-5 border-0 p-4 px-5 ms-4">Search courses</button>
+                    <button class="bg_light txt_danger fw-medium rounded-5 border-0 p-4 px-5 ms-4">Apply for
+                        university</button>
+                </div>
+            </div>
+        </section>
+        <section id="section_splitter"></section>
+        <section id="courses">
+            <div class="container">
+                <img width="48px" src="./assets/img/Books-icon.png" alt="books">
+                <div class="row">
+                    <div class="col-6 mt-3">
+                        <h2 class="txt_main ls_1 fw-bold">Thousands of courses for any type of student</h2>
+                        <p>At EduPrime, it doesn't matter what domain you wish to pursue a career in. Here you can find a
+                            course that satisfies your needs within a click away and applies for a course in a matter of
+                            minutes. EduPrime is ranked as the most versatile university in the world, thanks to the number
+                            of courses it provides</p>
+                            <button class="bg_warning txt_white fw-medium rounded-5 border-0 mt-4 p-4 px-5">Browse through courses</button>
+                    </div>
+                    <div class="col-6">
+                        <img class="img-fluid" src="./assets/img/Graduation-Illustration.png" alt="">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </section>
 
     </main>
-    
+
     <footer>
 
     </footer>
-  
 </template>
 
 
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './assets/scss/app.scss'
 import App from './App.vue'
+import * as bootstrap from 'bootstrap'
 
-createApp(App).mount('#app')
+createApp(App)
+.component('bootstrap', bootstrap)
+.mount('#app')
