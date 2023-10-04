f1d7614a33680bc832f36b8ad5ed67045efcb3c6
Francesco-Munafo <acquamarea1@gmail.com>
2023-10-04 11:23:39 +0200

    Add events section

--- src/App.vue
+++ src/App.vue
@@ -72,7 +72,7 @@
                             <a class="nav-link txt_white" href="#">Purchase</a>
                         </li>
                     </ul>
-                    <button class="rounded-5 border-0 p-2 px-3 ms-4">VIEW CONCTACTS</button>
+                    <button class="rounded-5 border-0 p-2 px-3 ms-4">VIEW CONTACTS</button>
                 </div>
             </div>
         </nav>
@@ -80,15 +80,18 @@
     </header>
 
     <main>
-        <section id="jumbotron" class="d-flex justify-content-center text-center pt-5">
-            <div class="col-4">
-                <h1 class="fw-bold ls_1">Key to your success</h1>
-                <p class=" fs-5">EduPrime is the most versatile WordPress theme for educational purposes, showcasing
-                    universities, courses, secondary schools etc.</p>
-                <div class="d-flex my-5 justify-content-center">
-                    <button class="bg_warning txt_white fw-medium rounded-5 border-0 p-4 px-5 ms-4">Search courses</button>
-                    <button class="bg_light txt_danger fw-medium rounded-5 border-0 p-4 px-5 ms-4">Apply for
-                        university</button>
+        <section id="jumbotron">
+            <div class="background_overlay d-flex justify-content-center text-center pt-5">
+                <div class="col-4">
+                    <h1 class="fw-bold ls_1">Key to your success</h1>
+                    <p class=" fs-5">EduPrime is the most versatile WordPress theme for educational purposes, showcasing
+                        universities, courses, secondary schools etc.</p>
+                    <div class="d-flex my-5 justify-content-center">
+                        <button class="bg_warning txt_white fw-medium rounded-5 border-0 p-4 px-5 ms-4">Search
+                            courses</button>
+                        <button class="bg_light txt_danger fw-medium rounded-5 border-0 p-4 px-5 ms-4">Apply for
+                            university</button>
+                    </div>
                 </div>
             </div>
         </section>
@@ -117,7 +120,7 @@
             </div>
         </section>
 
-        <section id="faculties">
+        <section id="faculties" class="py-5">
             <div class="row m-0 justify-content-center">
                 <div class="col text-center p-0">
                     <h2 class="txt_main fw-bold">Faculties available at EduPrime</h2>
@@ -153,6 +156,59 @@
                     </div>
                 </div>
             </div>
+            <div class="container faculty">
+                <div class="row row-cols-2 justify-content-center">
+                    <div class="col text-center">
+                        <img src="./assets/img/Gavel-Illustration-e1556884768193.png" alt="">
+                    </div>
+                    <div class="col">
+                        <h2 class="txt_main fw-bold mb-4">Law Faculty</h2>
+                        <p>Learning from world-leading academics and practitioners, you'll not only receive an outstanding
+                            grounding in the theory of law, but you will be able to understand how those principles are
+                            applied in practice through a range of student-led activities and competitions.</p>
+                        <button class="bg_danger txt_white fw-medium rounded-5 border-0 mt-4 p-4 px-5">Read more</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="faculties_splitter"></section>
+
+        <section id="events">
+            <div class="row justify-content-center m-0 ">
+                <div class="col d-flex flex-column align-items-center">
+                    <img class="d-block pb-3" width="48px" src="./assets/img/Clock-and-Bell.png" alt="">
+                    <h2 class="fw-bold txt_white bg-danger d-inline-block m-auto">University Year</h2>
+
+                    <div class="timeline">
+
+                        <div class="orientation description">
+                            <h5>Orientation</h5>
+                           <p>First day of the university year, all students gather for the opening ceremony and then network with others.</p> 
+                        </div>
+
+                        <div class="demo description">
+                           <h5>Demo Classes</h5>
+                           <p>In the first week, students try to accomodate with the teaching style and choose their optional courses.</p> 
+                        </div>
+
+                        <div class="evaluation description">
+                           <h5>Evaluation</h5>
+                           <p>At the end of a semester, students take a general evaluation test for every subject they're learning.</p> 
+                        </div>
+
+                        <div class="graduation description">
+                           <h5>Graduation Day</h5>
+                           <p>At the end of a semester, students take a general evaluation test for every subject they're learning.</p> 
+                        </div>
+
+                        
+                        <img src="./assets/img/Timeline-Item.png" alt="">
+                    </div>
+
+                </div>
+            </div>
+
         </section>
 
     </main>
